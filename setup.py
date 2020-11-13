ee19dd888384485c589d017a9e4f04951ef2dd11
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2020-11-13 12:40:26 +0100

    fixed setup

--- setup.py
+++ setup.py
@@ -4,29 +4,28 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(
-      author='Alessandro Puzielli aka Alepuzio',
-      author_email='alessandro.puzielli@alepuzio.net',
+setup(name='calculate-priority-activities',
+      version='1.0.4',      
+      description='This script prints a file with the activities with their priority in the root directory',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
       classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: GPL 3 License',
+        'Development Status :: 5 - Production/Stable'
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
         'Programming Language :: Python :: 3',
         'Topic :: Text Processing',
       ],
-      description='This script prints a file with the activities with their priority in the root directory',
-      entry_points={
-          'console_scripts': ['activities=funniest.command_line:main'],
-      },
-      include_package_data=True,
+      url='calculate-priority-activities',
+      author='Alessandro Puzielli aka Alepuzio',
+      author_email='alessandro.puzielli@alepuzio.net',
+      license='GPL 3',
+      packages=['src'],
       install_requires=[
           'markdown',
       ],
-      license='GPL 3',
-	  long_description=readme(),
-      name='calculate-priority-activities',
-      packages=setuptools.find_packages(),
-      script=['calculate-priority-activities'],
+      entry_points={
+          'console_scripts': ['calculate-priority-activities=main'],
+      },
+      include_package_data=True,
       url='http://github.com/alepuzio/calculate-priority-activities',
-      version='0.1',      
       zip_safe=False)
-
